22422ad26c23b92c8be16159598d769846269e44
EnzoVonkunoschy <enzovonkunoschy@gmail.com>
2019-03-17 23:06:25 -0300

    More

---
+++ public/aula/script01.js
@@ -0,0 +1,7 @@
+
+	//alert("ejecutando pie");
+	document.getElementById("pieDePagina").innerHTML = '<footer><div align="center"><table class="pie"><!--tr><td class="izq"><img src="../wpp_1.png"</td></td> <td class="der"><big>+54 9 261 2 443928</big></td></tr--></table><p>CCT 6-303 - Escuela Francisca Godoy de Molina - Tel: 4941993 - Fleming 164 - Villa Tulumaya</p> </div></footer>';
+
+function pie(){
+
+}
